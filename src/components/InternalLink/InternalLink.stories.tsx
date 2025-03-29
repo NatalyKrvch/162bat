e5599032abab02a327c638c5a1d888b2afabbea5
Kuzmenko Anna <anna.kuzmenko120063@gmail.com>
2025-03-29 13:51:47 -0700

    feat: add Storybook stories for InternalLink component

---
+++ src/components/InternalLink/InternalLink.stories.tsx
@@ -0,0 +1,37 @@
+import { action } from '@storybook/addon-actions';
+import { Meta, StoryObj } from '@storybook/react';
+
+import InternalLink from './InternalLink';
+
+export default {
+  component: InternalLink,
+  title: 'Internal Link',
+  tags: ['autodocs'],
+  argTypes: {
+    title: { control: 'text' },
+    href: { control: 'text' },
+    className: { control: 'text' },
+    children: {
+      control: 'text',
+      table: { defaultValue: { summary: 'undefined' } },
+    },
+    onClick: action('clicked'),
+    onMouseEnter: action('hovered'),
+  },
+} as Meta;
+
+export const Default: StoryObj = {
+  args: {
+    title: 'Головна',
+    href: '/home',
+    className: '',
+  },
+};
+
+export const Underlined: StoryObj = {
+  args: {
+    title: 'Читати повністю',
+    href: '/history_of_the_batalion',
+    className: 'underline',
+  },
+};
