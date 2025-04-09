420d83596bfc8aca73a4c5fa94236e5a39327d7c
Kuzmenko Anna <anna.kuzmenko120063@gmail.com>
2025-04-09 09:34:29 +0300

    feat: add Divider component with gradient and grey variants

---
+++ src/components/Divider/types.ts
@@ -0,0 +1,3 @@
+export interface DividerProps {
+  variant?: 'grey' | 'gradient';
+}
--- src/lib/testIds.ts
+++ src/lib/testIds.ts
@@ -1,2 +1,3 @@
 export const INTERNAL_LINK_TEST_ID = 'internal-link';
 export const EXTERNAL_LINK_TEST_ID = 'external-link';
+export const DIVIDER_TEST_ID = 'external-link';
