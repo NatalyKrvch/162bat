ab81b1adeffe23a92aa91d121fb76db012088787
Kuzmenko Anna <anna.kuzmenko120063@gmail.com>
2025-04-10 22:32:18 +0300

    docs: add stories for BasicCard component with default and gradient variants

---
+++ src/components/BasicCard/BasicCard.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import BasicCard from './BasicCard';
+
+export default {
+  component: BasicCard,
+  title: 'Basic Card',
+  tags: ['autodocs'],
+  argTypes: {
+    gradientPosition: {
+      control: 'radio',
+      options: ['top', 'left'],
+    },
+    className: {
+      control: false,
+    },
+    children: {
+      control: 'text',
+    },
+  },
+} as Meta;
+
+export const Default: StoryObj = {
+  args: {
+    children: 'This card has default gradient border.',
+  },
+};
+
+export const TopGradient: StoryObj = {
+  args: {
+    gradientPosition: 'top',
+    children: 'This card has a top gradient border.',
+  },
+};
+
+export const LeftGradient: StoryObj = {
+  args: {
+    gradientPosition: 'left',
+    children: 'This card has a left gradient border.',
+  },
+};
