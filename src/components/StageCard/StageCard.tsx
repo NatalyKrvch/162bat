70273ca612e64a4261d333b03b927d72c1ffab0a
Kuzmenko Anna <anna.kuzmenko120063@gmail.com>
2025-04-15 22:28:02 +0300

    feat: add StageCard component

---
+++ src/components/StageCard/StageCard.tsx
@@ -0,0 +1,20 @@
+import { STAGE_CARD_TEST_ID } from '@/lib/testIds';
+
+import { StageCardProps } from './types';
+
+const StageCard = ({ step, title, description }: StageCardProps) => {
+  return (
+    <div
+      data-testid={STAGE_CARD_TEST_ID}
+      className="rounded-default border-grey bg-bg-secondary flex w-full items-center gap-4 border-1 px-8 py-6"
+    >
+      <p className="rotated text-description text-4xl-40 font-bold">{step}</p>
+      <div className="flex flex-col gap-3">
+        <h3 className="text-2xl/7 font-bold">{title}</h3>
+        <p className="text-lg/6">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default StageCard;
