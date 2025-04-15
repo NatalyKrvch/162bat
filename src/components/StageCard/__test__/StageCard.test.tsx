65cdbc839a0d4421e4fa0ddf0600936b9c469c13
Kuzmenko Anna <anna.kuzmenko120063@gmail.com>
2025-04-15 22:31:13 +0300

    test: add unit tests and snapshots for StageCard component

---
+++ src/components/StageCard/__test__/StageCard.test.tsx
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom';
+
+import { render } from '@testing-library/react';
+
+import { STAGE_CARD_TEST_ID } from '@/lib/testIds';
+
+import StageCard from '../StageCard';
+import { StageCardProps } from '../types';
+
+const mockData: StageCardProps = {
+  step: 'КРОК 1',
+  title: 'Залишаєте заявку',
+  description:
+    "Просто заповніть форму, і ми з вами зв'яжемося найближчим часом.",
+};
+
+describe('StageCard', () => {
+  it('renders correctly with valid data', () => {
+    const { container, getByText, getByTestId } = render(
+      <StageCard {...mockData} />,
+    );
+
+    const stageCard = getByTestId(STAGE_CARD_TEST_ID);
+    expect(stageCard).toBeInTheDocument();
+    expect(getByText('КРОК 1')).toBeInTheDocument();
+    expect(getByText('Залишаєте заявку')).toBeInTheDocument();
+    expect(
+      getByText(
+        "Просто заповніть форму, і ми з вами зв'яжемося найближчим часом.",
+      ),
+    ).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+});
