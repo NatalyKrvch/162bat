9f1424ea9fdd53546b1ca96e4060091785fd4564
Kuzmenko Anna <anna.kuzmenko120063@gmail.com>
2025-04-15 22:31:01 +0300

    docs: add stories for StageCard component

---
+++ src/components/StageCard/StageCard.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import StageCard from './StageCard';
+
+export default {
+  component: StageCard,
+  title: 'Stage Card',
+  tags: ['autodocs'],
+  args: {
+    step: 'КРОК 1',
+    title: 'Залишаєте заявку',
+    description:
+      "Просто заповніть форму, і ми з вами зв'яжемося найближчим часом.",
+  },
+} as Meta;
+
+export const Default: StoryObj = {
+  args: {},
+};
