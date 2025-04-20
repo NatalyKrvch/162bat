83d74bb3f2a1f14e760473ba8654da9047bdf49d
Kuzmenko Anna <anna.kuzmenko120063@gmail.com>
2025-04-20 23:09:14 +0300

    docs: update Carousel stories

--- src/components/Carousel/Carousel.stories.tsx
+++ src/components/Carousel/Carousel.stories.tsx
@@ -10,14 +10,14 @@ export default {
 } as Meta;
 
 const slides2 = Array.from({ length: 5 }, (_, index) => (
-  <div key={index} className="keen-slider__slide grid grid-rows-2 gap-6">
+  <div className="grid grid-rows-2 gap-6" key={index}>
     <BasicCard gradientPosition="left">Slide {index * 2 + 1}</BasicCard>
     <BasicCard gradientPosition="left">Slide {index * 2 + 2}</BasicCard>
   </div>
 ));
 
 const slides3 = Array.from({ length: 5 }, (_, index) => (
-  <div key={index} className="keen-slider__slide grid grid-cols-3 gap-6">
+  <div className="grid grid-cols-3 gap-6" key={index}>
     <BasicCard gradientPosition="left">Slide {index * 3 + 1}</BasicCard>
     <BasicCard gradientPosition="left">Slide {index * 3 + 2}</BasicCard>
     <BasicCard gradientPosition="left">Slide {index * 3 + 3}</BasicCard>
@@ -25,7 +25,7 @@ const slides3 = Array.from({ length: 5 }, (_, index) => (
 ));
 
 const slides4 = Array.from({ length: 5 }, (_, index) => (
-  <div key={index} className="keen-slider__slide grid grid-cols-2 gap-6">
+  <div className="grid grid-cols-2 gap-6" key={index}>
     <BasicCard gradientPosition="left">Slide {index * 4 + 1}</BasicCard>
     <BasicCard gradientPosition="left">Slide {index * 4 + 2}</BasicCard>
     <BasicCard gradientPosition="left">Slide {index * 4 + 3}</BasicCard>
