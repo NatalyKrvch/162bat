87f09e926a97d2c80c252bd5ddeb4082baa89cb0
Anastasia <kop.anastasia27@gmail.com>
2025-06-03 20:59:13 +0300

    feat: add borderless card variant

--- src/components/BasicCard/BasicCard.tsx
+++ src/components/BasicCard/BasicCard.tsx
@@ -6,20 +6,30 @@ import { BASIC_CARD_TEST_ID } from '@/lib/testIds';
 import { BasicCardProps } from './types';
 
 const BasicCard = ({
-  gradientPosition = 'top',
+  gradientVariant = 'top',
   className = '',
   children,
 }: PropsWithChildren<BasicCardProps>) => {
-  const isTopGradient = gradientPosition === 'top';
-
   return (
     <article
       data-testid={BASIC_CARD_TEST_ID}
-      className={`rounded-default h-full w-full ${isTopGradient ? 'bg-card-gradient-top pt-px' : 'bg-card-gradient-left pl-px'}`}
+      className={twMerge(
+        'rounded-default h-full w-full',
+        gradientVariant === 'borderless'
+          ? 'pt-0 pl-0'
+          : gradientVariant === 'top'
+            ? 'bg-card-gradient-top pt-px'
+            : 'bg-card-gradient-left pl-px',
+      )}
     >
       <div
         className={twMerge(
-          `bg-bg-secondary border-grey rounded-inner-card h-full w-full border-1 p-8 ${isTopGradient ? 'border-t-0' : 'border-l-0'}`,
+          'bg-bg-secondary border-grey rounded-inner-card h-full w-full border-1 p-8',
+          gradientVariant === 'borderless'
+            ? 'border-0'
+            : gradientVariant === 'top'
+              ? 'border-t-0'
+              : 'border-l-0',
           className,
         )}
       >
