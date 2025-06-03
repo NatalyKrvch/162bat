e30f00eb453870aa7accd3f199546d3088609cf4
Anastasia <kop.anastasia27@gmail.com>
2025-06-03 22:12:18 +0300

    fix: update stories to reflect new BasicCard changes

--- src/components/Carousel/Carousel.stories.tsx
+++ src/components/Carousel/Carousel.stories.tsx
@@ -11,25 +11,25 @@ export default {
 
 const slides2 = Array.from({ length: 5 }, (_, index) => (
   <div className="grid grid-rows-2 gap-6" key={index}>
-    <BasicCard gradientPosition="left">Slide {index * 2 + 1}</BasicCard>
-    <BasicCard gradientPosition="left">Slide {index * 2 + 2}</BasicCard>
+    <BasicCard gradientVariant="left">Slide {index * 2 + 1}</BasicCard>
+    <BasicCard gradientVariant="left">Slide {index * 2 + 2}</BasicCard>
   </div>
 ));
 
 const slides3 = Array.from({ length: 5 }, (_, index) => (
   <div className="grid grid-cols-3 gap-6" key={index}>
-    <BasicCard gradientPosition="left">Slide {index * 3 + 1}</BasicCard>
-    <BasicCard gradientPosition="left">Slide {index * 3 + 2}</BasicCard>
-    <BasicCard gradientPosition="left">Slide {index * 3 + 3}</BasicCard>
+    <BasicCard gradientVariant="left">Slide {index * 3 + 1}</BasicCard>
+    <BasicCard gradientVariant="left">Slide {index * 3 + 2}</BasicCard>
+    <BasicCard gradientVariant="left">Slide {index * 3 + 3}</BasicCard>
   </div>
 ));
 
 const slides4 = Array.from({ length: 5 }, (_, index) => (
   <div className="grid grid-cols-2 gap-6" key={index}>
-    <BasicCard gradientPosition="left">Slide {index * 4 + 1}</BasicCard>
-    <BasicCard gradientPosition="left">Slide {index * 4 + 2}</BasicCard>
-    <BasicCard gradientPosition="left">Slide {index * 4 + 3}</BasicCard>
-    <BasicCard gradientPosition="left">Slide {index * 4 + 4}</BasicCard>
+    <BasicCard gradientVariant="left">Slide {index * 4 + 1}</BasicCard>
+    <BasicCard gradientVariant="left">Slide {index * 4 + 2}</BasicCard>
+    <BasicCard gradientVariant="left">Slide {index * 4 + 3}</BasicCard>
+    <BasicCard gradientVariant="left">Slide {index * 4 + 4}</BasicCard>
   </div>
 ));
 
