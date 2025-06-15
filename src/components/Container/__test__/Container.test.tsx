4cb6f1e59ed055b15dace849a3729aabb941c9ce
Kuzmenko Anna <anna.kuzmenko120063@gmail.com>
2025-06-15 16:21:49 +0300

    test: add tests and snapshot for Container component

---
+++ src/components/Container/__test__/Container.test.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+
+import { CONTAINER_TEST_ID } from '@/lib/testIds';
+
+import Container from '../Container';
+
+describe('Container', () => {
+  it('renders children correctly and matches snapshot', () => {
+    const { container, getByTestId, getByText } = render(
+      <Container>
+        <p>Test content</p>
+      </Container>,
+    );
+
+    const containerComponent = getByTestId(CONTAINER_TEST_ID);
+    const content = getByText('Test content');
+
+    expect(content).toBeInTheDocument();
+    expect(containerComponent).toBeInTheDocument();
+    expect(containerComponent).toHaveClass(
+      'mx-auto',
+      'max-w-screen-2xl',
+      'px-4',
+      'md:px-8',
+      'xl:px-20',
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
