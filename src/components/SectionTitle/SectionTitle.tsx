3751817e448891cc922472ef557a6da925f01404
Kuzmenko Anna <anna.kuzmenko120063@gmail.com>
2025-06-19 08:08:05 +0300

    refactor: add className prop for SectionTitle component

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,12 +1,21 @@
 import { type PropsWithChildren } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 import { SECTION_TITLE_TEST_ID } from '@/lib/testIds';
 
-const SectionTitle = ({ children }: PropsWithChildren) => {
+import { SectionTitleProps } from './types';
+
+const SectionTitle = ({
+  className = '',
+  children,
+}: PropsWithChildren<SectionTitleProps>) => {
   return (
     <h2
       data-testid={SECTION_TITLE_TEST_ID}
-      className="w-full text-center text-2xl/7 font-bold uppercase sm:text-5xl/13 md:text-6xl/16"
+      className={twMerge(
+        'w-full text-center text-2xl/7 font-bold uppercase sm:text-5xl/13 md:text-6xl/16',
+        className,
+      )}
     >
       {children}
     </h2>
---
+++ src/components/SectionTitle/types.ts
@@ -0,0 +1,3 @@
+export interface SectionTitleProps {
+  className?: string;
+}
