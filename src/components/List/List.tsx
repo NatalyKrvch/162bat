04b5443419092f57ceb454561208d2ed867c9405
Anastasia <kop.anastasia27@gmail.com>
2025-06-19 14:23:19 +0300

    refactor: fix import, reorder props in component and types, extract image from conponent, extract style for readability

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,51 +1,47 @@
 import Image from 'next/image';
 import { twMerge } from 'tailwind-merge';
 
-import { LIST_TEST_ID } from '@/lib/testIds';
+import { LIST_TEST_ID, STAR_ICON_TEST_ID } from '@/lib/testIds';
+import { getFormattedText } from '@/utils/getFormattedText';
 
-import { getFormattedText } from '../../utils/getFormattedText';
 import { ListProps } from './types';
 
+const STAR_ICON = (
+  <Image
+    src="/icons/star.svg"
+    alt="star icon"
+    width={24}
+    height={24}
+    className="h-4 w-4 min-w-4 sm:h-6 sm:w-6 sm:min-w-6"
+    data-testid={STAR_ICON_TEST_ID}
+  />
+);
+
 const List = ({
+  listItems,
   className = '',
   bulletType = 'circle',
-  listItems,
 }: ListProps) => {
-  if (!listItems || listItems.length === 0) return null;
+  if (listItems.length === 0) return null;
 
   const isGreenStar = bulletType === 'star';
 
-  const greenStarSvg = (
-    <Image
-      src="/icons/star.svg"
-      alt="star icon"
-      width={24}
-      height={24}
-      className="h-4 w-4 min-w-4 sm:h-6 sm:w-6 sm:min-w-6"
-      data-testid="star-icon"
-    />
+  const listStyle = twMerge(
+    isGreenStar
+      ? 'flex flex-col gap-4 text-base sm:text-2xl'
+      : 'list-disc pl-4 text-base sm:text-xl',
+    className,
   );
 
+  const listItemsStyle = isGreenStar
+    ? 'flex items-center gap-4 lining-nums tabular-nums'
+    : '';
+
   return (
-    <ul
-      data-testid={LIST_TEST_ID}
-      className={twMerge(
-        isGreenStar
-          ? 'flex flex-col gap-4 text-base sm:text-2xl'
-          : 'list-disc pl-4 text-base sm:text-xl',
-        className,
-      )}
-    >
+    <ul data-testid={LIST_TEST_ID} className={listStyle}>
       {listItems.map(item => (
-        <li
-          key={item.id}
-          className={
-            isGreenStar
-              ? 'flex items-center gap-4 lining-nums tabular-nums'
-              : ''
-          }
-        >
-          {isGreenStar && greenStarSvg}
+        <li key={item.id} className={listItemsStyle}>
+          {isGreenStar && STAR_ICON}
           {typeof item.children === 'string'
             ? item.children
             : getFormattedText(item.children)}
