4d137788c1c328fd19271bf0201c726620d80671
Anastasia <kop.anastasia27@gmail.com>
2025-06-30 20:21:18 +0300

    feat: implement open/close animation, remove fragment, add poinet and action to the whole header

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -11,35 +11,32 @@ const Accordion = ({ accordionItems }: AccordionProps) => {
   if (accordionItems.length === 0) return null;
 
   return (
-    <>
-      <AccordionRadix.Root
-        type="single"
-        defaultValue={accordionItems[0].id}
-        collapsible
-        className="flex flex-col gap-4"
-        data-testid={ACCORDION_TEST_ID}
-      >
-        {accordionItems.map(({ id, title, content }) => (
-          <AccordionRadix.Item value={id} key={id} className="group">
-            <BasicCard gradientPosition="left" className="p-6 sm:p-8 md:py-10">
-              <AccordionRadix.Header>
-                <AccordionRadix.Trigger className="group-data-[state=open]:text-main flex w-full items-center justify-between text-lg font-bold sm:text-2xl">
-                  {title}
-                  <Chevron className="group-data-[state=open]:stroke-main transition-transform duration-200 group-data-[state=open]:rotate-180" />
-                </AccordionRadix.Trigger>
-              </AccordionRadix.Header>
-              <AccordionRadix.Content
-                data-testid={`accordion-content-${id}`}
-                className="mt-6 flex flex-col gap-6 sm:mt-8 sm:gap-8 md:mt-10 md:gap-10"
-              >
-                <Divider />
-                {content}
-              </AccordionRadix.Content>
-            </BasicCard>
-          </AccordionRadix.Item>
-        ))}
-      </AccordionRadix.Root>
-    </>
+    <AccordionRadix.Root
+      type="single"
+      collapsible
+      className="flex flex-col gap-4"
+      data-testid={ACCORDION_TEST_ID}
+    >
+      {accordionItems.map(({ id, title, content }) => (
+        <AccordionRadix.Item value={id} key={id} className="group">
+          <BasicCard gradientPosition="left" className="p-0">
+            <AccordionRadix.Header>
+              <AccordionRadix.Trigger className="group-data-[state=open]:text-main flex w-full cursor-pointer items-center justify-between p-6 text-lg font-bold sm:p-8 sm:text-2xl md:py-10">
+                {title}
+                <Chevron className="group-data-[state=open]:stroke-main transition-transform duration-300 group-data-[state=open]:rotate-180" />
+              </AccordionRadix.Trigger>
+            </AccordionRadix.Header>
+            <AccordionRadix.Content
+              data-testid={`accordion-content-${id}`}
+              className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down flex flex-col gap-6 p-6 pt-0 sm:gap-8 sm:p-8 sm:pt-0 md:gap-10 md:pt-0 md:pb-10"
+            >
+              <Divider />
+              {content}
+            </AccordionRadix.Content>
+          </BasicCard>
+        </AccordionRadix.Item>
+      ))}
+    </AccordionRadix.Root>
   );
 };
 
