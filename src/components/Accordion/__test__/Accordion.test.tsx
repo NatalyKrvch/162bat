48e7e94d2567d1434ce0c3d9121fd3f53f4853cb
Anastasia <kop.anastasia27@gmail.com>
2025-06-30 20:30:29 +0300

    test: remove unnecessary test and async, refactor

--- src/components/Accordion/__test__/Accordion.test.tsx
+++ src/components/Accordion/__test__/Accordion.test.tsx
@@ -13,56 +13,36 @@ describe('Accordion', () => {
 
     const accordion = getByTestId(ACCORDION_TEST_ID);
     expect(accordion).toBeInTheDocument();
-    expect(accordion).toHaveClass('flex flex-col gap-4');
 
     accordionItems.forEach(item => {
       expect(accordion).toHaveTextContent(item.title);
     });
-    expect(accordion).toHaveClass('flex flex-col gap-4');
 
     expect(container).toMatchSnapshot();
   });
 
-  it('should open the first accordion item by default', () => {
-    const { container, getByTestId } = render(
-      <Accordion accordionItems={accordionItems} />,
-    );
-
-    const firstItemContent = getByTestId(
-      `accordion-content-${accordionItems[0].id}`,
-    );
-    expect(firstItemContent).toHaveAttribute('data-state', 'open');
-
-    if (typeof firstItemContent === 'string') {
-      expect(firstItemContent).toHaveTextContent(accordionItems[0].content);
-    }
-
-    expect(container).toMatchSnapshot();
-  });
-
-  it('should open the correct item on click and close others', async () => {
+  it('should open the correct item on click and close others', () => {
     const { getByTestId } = render(
       <Accordion accordionItems={accordionItems} />,
     );
 
-    for (let i = 1; i < accordionItems.length; i++) {
-      const currentItem = accordionItems[i];
-      const trigger = screen.getByText(currentItem.title);
+    for (const accordionItem of accordionItems) {
+      const trigger = screen.getByText(accordionItem.title);
 
       fireEvent.click(trigger);
 
-      const opened = getByTestId(`accordion-content-${currentItem.id}`);
+      const opened = getByTestId(`accordion-content-${accordionItem.id}`);
       expect(opened).toHaveAttribute('data-state', 'open');
 
-      if (typeof currentItem.content === 'string') {
-        expect(opened).toHaveTextContent(currentItem.content);
+      if (typeof accordionItem.content === 'string') {
+        expect(opened).toHaveTextContent(accordionItem.content);
       }
 
       const allContentBlocks = screen.getAllByTestId(/accordion-content-/);
       const closedItems = allContentBlocks.filter(
         item =>
           item.getAttribute('data-testid') !==
-          `accordion-content-${currentItem.id}`,
+          `accordion-content-${accordionItem.id}`,
       );
       closedItems.forEach(item => {
         expect(item).toHaveAttribute('data-state', 'closed');
