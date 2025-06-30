f58cb3139cb6e30627fd075da8d5c459d554e091
Anastasia <kop.anastasia27@gmail.com>
2025-06-30 23:38:20 +0300

    feat: add direction props to the Chevron component, update styles

--- src/components/icons/Chevron/Chevron.tsx
+++ src/components/icons/Chevron/Chevron.tsx
@@ -4,10 +4,21 @@ import { CHEVRON_TEST_ID } from '@/lib/testIds';
 
 import type { ChevronProps } from './types';
 
-const Chevron = ({ className }: ChevronProps) => {
+const Chevron = ({ className, direction = 'down' }: ChevronProps) => {
+  const rotationClass = {
+    down: '',
+    up: 'rotate-180',
+    left: 'rotate-90',
+    right: '-rotate-90',
+  }[direction];
+
   return (
     <svg
-      className={twMerge('h-6 w-6 stroke-white sm:h-8 sm:w-8', className)}
+      className={twMerge(
+        'h-6 w-6 stroke-white transition-transform duration-300 sm:h-8 sm:w-8',
+        rotationClass,
+        className,
+      )}
       viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
