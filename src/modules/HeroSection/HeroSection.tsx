47a5d90090b3109374a95bc0a767edcdd5925ab8
Kuzmenko Anna <anna.kuzmenko120063@gmail.com>
2025-07-02 23:41:39 +0300

    refactor: optimize HeroSection component structure and ensure Container is used correctly

--- src/modules/HeroSection/HeroSection.tsx
+++ src/modules/HeroSection/HeroSection.tsx
@@ -2,31 +2,29 @@ import { Button, Container, InternalLink } from '@/components';
 import { HERO_SECTION_TEST_ID } from '@/lib/testIds';
 import { getFormattedText } from '@/utils';
 
-import { HeroSectionProps } from './types';
+import type { HeroSectionProps } from './types';
 
 const HeroSection = ({ data }: HeroSectionProps) => {
-  const { title, button } = data;
-
   if (!data) {
     return null;
   }
 
+  const { title, button } = data;
+
   return (
-    <div className="bg-hero-section min-h-screen-minus-header w-screen">
-      <Container>
-        <section
-          data-testid={HERO_SECTION_TEST_ID}
-          className="min-h-screen-minus-header flex flex-col items-center justify-center gap-10"
-        >
-          <h1 className="max-w-3xl text-center text-4xl sm:text-6xl">
-            {getFormattedText(title)}
-          </h1>
-          <InternalLink href={button.url}>
-            <Button>{button.title}</Button>
-          </InternalLink>
-        </section>
-      </Container>
-    </div>
+    <Container fullWidth className="bg-hero-section min-h-screen-minus-header">
+      <section
+        data-testid={HERO_SECTION_TEST_ID}
+        className="min-h-screen-minus-header flex flex-col items-center justify-center gap-10"
+      >
+        <h1 className="max-w-3xl text-center text-4xl sm:text-6xl">
+          {getFormattedText(title)}
+        </h1>
+        <InternalLink href={button.url}>
+          <Button>{button.title}</Button>
+        </InternalLink>
+      </section>
+    </Container>
   );
 };
 
