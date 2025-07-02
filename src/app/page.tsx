398d5f66f876cdb0561cfb77b708232714f777fc
Kuzmenko Anna <anna.kuzmenko120063@gmail.com>
2025-07-02 23:42:05 +0300

    feat: implement Home component with HeroSection and GuaranteesSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+
 import historySectionData from '@/data/historySectionData.json';
 import HistorySection from '@/modules/HistorySection/HistorySection';
+import { guaranteesSectionData, heroSectionData } from '@/data';
+import { GuaranteesSection, HeroSection } from '@/modules';
 
 const Home = () => {
-  return <HistorySection data={historySectionData} />;
+  return (
+    <div className="mb-20 flex flex-col items-center gap-20">
+      <HeroSection data={heroSectionData} />
+      <HistorySection data={historySectionData} />
+      <GuaranteesSection data={guaranteesSectionData} />
+    </div>
+  );
 };
 
 export default Home;
