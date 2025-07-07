47a37ff27ade938faba5c7449b13646fd52c0efd
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-07-07 14:58:09 +0300

    fix: HistorySection styles & data

--- src/modules/HistorySection/HistorySection.tsx
+++ src/modules/HistorySection/HistorySection.tsx
@@ -34,7 +34,7 @@ const HistorySection = ({ data }: HistorySectionProps) => {
           </InternalLink>
         </div>
 
-        <p className="mb-10 max-w-240 text-base leading-[150%] lg:mb-8 lg:text-lg">
+        <p className="mb-10 max-w-240 text-base leading-[150%] lg:mb-8 lg:text-lg xl:mb-10">
           {introText}
         </p>
 
