984914538e218570456850bcd8379ae299fdf0cc
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-07-15 00:58:09 +0300

    test: update section tests

--- src/modules/HeroSection/__test__/HeroSection.test.tsx
+++ src/modules/HeroSection/__test__/HeroSection.test.tsx
@@ -1,27 +1,37 @@
 import { render } from '@testing-library/react';
 
-import heroSectionData from '@/data/heroSectionData.json';
 import { HERO_SECTION_TEST_ID, INTERNAL_LINK_TEST_ID } from '@/lib/testIds';
 
 import HeroSection from '../HeroSection';
 
 describe('HeroSection', () => {
-  const { title, button } = heroSectionData;
+  const mockData = {
+    sectionTitle: [
+      { id: '1', text: 'text1', color: 'white', fontWeight: 'bold' },
+      { id: '2', text: 'text2 ', color: 'white', fontWeight: 'bold' },
+      { id: '3', text: 'text3', color: 'white', fontWeight: 'bold' },
+    ],
+    buttonTitle: 'buttonTitle',
+    buttonURL: 'buttonURL',
+  };
 
   it('renders correctly and matches snapshot', () => {
-    const { container, getByTestId } = render(
-      <HeroSection data={heroSectionData} />,
-    );
+    const { container, getByTestId } = render(<HeroSection data={mockData} />);
+
     const section = getByTestId(HERO_SECTION_TEST_ID);
+
     expect(section).toBeInTheDocument();
     expect(getByTestId(INTERNAL_LINK_TEST_ID)).toHaveAttribute(
       'href',
-      button.url,
+      mockData.buttonURL,
     );
-    title.forEach(({ text }) => {
+
+    mockData.sectionTitle.forEach(({ text }) => {
       expect(section).toHaveTextContent(text);
     });
-    expect(section).toHaveTextContent(button.title.trim());
+
+    expect(section).toHaveTextContent(mockData.buttonTitle.trim());
+
     expect(container).toMatchSnapshot();
   });
 });
