f7bd48e470519901c13d798bff1f75b8ba701ed2
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-07-15 00:54:39 +0300

    refactor: section styles

--- src/modules/HeroSection/HeroSection.tsx
+++ src/modules/HeroSection/HeroSection.tsx
@@ -9,19 +9,25 @@ const HeroSection = ({ data }: HeroSectionProps) => {
     return null;
   }
 
-  const { title, button } = data;
+  const { sectionTitle, buttonTitle, buttonURL } = data;
 
   return (
-    <Container fullWidth className="bg-hero-section min-h-screen-minus-header">
+    <Container
+      fullWidth
+      className="min-h-screen-minus-header relative overflow-hidden"
+    >
+      <div className="bg-hero-section absolute inset-0 opacity-70"></div>
       <section
         data-testid={HERO_SECTION_TEST_ID}
-        className="min-h-screen-minus-header flex flex-col items-center justify-center gap-10"
+        className="min-h-screen-minus-header relative z-10 flex flex-col items-center justify-center gap-10"
       >
-        <h1 className="max-w-3xl text-center text-4xl sm:text-6xl">
-          {getFormattedText(title)}
+        <h1 className="text-4xl-40 max-w-3xl cursor-default text-center leading-none font-bold tracking-normal md:text-6xl lg:text-[64px]">
+          {getFormattedText(sectionTitle)}
         </h1>
-        <InternalLink href={button.url}>
-          <Button>{button.title}</Button>
+        <InternalLink href={buttonURL}>
+          <Button className="text-xl leading-none font-bold tracking-normal md:text-2xl lg:text-2xl xl:text-2xl">
+            {buttonTitle}
+          </Button>
         </InternalLink>
       </section>
     </Container>
