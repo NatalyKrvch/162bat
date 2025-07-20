bcddefde6ef828b8e098ad8b38a7d73e7e8f3777
Anastasia <kop.anastasia27@gmail.com>
2025-07-20 21:23:28 +0300

    refactor: rename IconButton and made it atomic, changed duration of transition, move component to the Buttons dir, update stories and test"

---
+++ src/components/Buttons/IconButton/__test__/IconButton.test.tsx
@@ -0,0 +1,38 @@
+import { fireEvent, render } from '@testing-library/react';
+
+import { ICON_TEST_ID } from '@/lib/testIds';
+
+import IconButton from '../IconButton';
+
+const DummyIcon = <svg data-testid="dummy-icon" />;
+
+describe('Icon', () => {
+  it('should render component correctly', () => {
+    const { container, getByTestId } = render(<IconButton icon={DummyIcon} />);
+
+    const iconElement = getByTestId(ICON_TEST_ID);
+    expect(iconElement).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render with custom className', () => {
+    const { getByTestId } = render(
+      <IconButton icon={DummyIcon} className="custom-class" />,
+    );
+
+    const iconElement = getByTestId(ICON_TEST_ID);
+    expect(iconElement).toHaveClass('custom-class');
+  });
+
+  it('should call onClick handler when clicked', () => {
+    const handleClick = jest.fn();
+    const { getByTestId } = render(
+      <IconButton icon={DummyIcon} onClick={handleClick} />,
+    );
+
+    const iconElement = getByTestId(ICON_TEST_ID);
+    fireEvent.click(iconElement);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
