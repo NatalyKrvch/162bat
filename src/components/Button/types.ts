c746d4a46ba101eb1b3766e39d78f7202cf791d6
Anastasia <kop.anastasia27@gmail.com>
2025-07-21 13:31:37 +0300

    chore: resolve merge conflicts after pulling from main and restructure Buttons folder, update test snapshots

--- src/components/Button/types.ts
+++
@@ -1,8 +0,0 @@
-import { type ButtonHTMLAttributes } from 'react';
-
-export type ButtonVariant = 'primary' | 'secondary' | 'link' | 'icon';
-
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: ButtonVariant;
-  className?: string;
-}
--- src/components/IconWithText/types.ts
+++
@@ -1,8 +0,0 @@
-import { JSX } from 'react';
-
-export interface IconWithTextProps {
-  icon: JSX.Element;
-  text: string;
-  className?: string;
-  onClick?: () => void;
-}
