80c7e1089cd72a44ebdd636a2a137d7152d3ce1e
Anastasia <kop.anastasia27@gmail.com>
2025-08-13 15:31:50 +0300

    test(MenuItemLink): add tests for component

---
+++ src/components/Links/MenuItemLink/__test__/MenuItemLink.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+
+import { INTERNAL_LINK_TEST_ID } from '@/lib/testIds';
+
+import MenuItemLink from '../MenuItemLink';
+import { MenuItemLinkProps } from '../types';
+
+const defaultProps: MenuItemLinkProps = {
+  text: 'Link',
+  href: '/test',
+  className: 'custom-class',
+};
+
+describe('MenuItemLink', () => {
+  it('should render component correctly', () => {
+    const { container } = render(<MenuItemLink {...defaultProps} />);
+    const menuItemLink = screen.getByTestId(INTERNAL_LINK_TEST_ID);
+
+    expect(menuItemLink).toBeInTheDocument();
+    expect(menuItemLink).toHaveClass('hover:bg-(--color-bg-secondary) p-4');
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render correctly variant for footer', () => {
+    const { container } = render(
+      <MenuItemLink {...defaultProps} variant="footerItem" />,
+    );
+
+    const menuItemLink = screen.getByTestId(INTERNAL_LINK_TEST_ID);
+    expect(menuItemLink).toBeInTheDocument();
+    expect(menuItemLink).toHaveClass('hover:text-btn-text-hover-secondary');
+    expect(container).toMatchSnapshot();
+  });
+});
