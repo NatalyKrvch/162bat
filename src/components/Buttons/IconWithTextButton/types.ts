ed8dcdcc33df3ce97b7c8dfae609faf3f1ae221c
Anastasia <kop.anastasia27@gmail.com>
2025-08-26 10:55:51 +0300

    reafactor: rename props

--- src/components/Buttons/IconWithTextButton/types.ts
+++ src/components/Buttons/IconWithTextButton/types.ts
@@ -3,7 +3,7 @@ import type { JSX } from 'react';
 export interface IconWithTextButtonProps {
   icon: JSX.Element;
   text: string;
-  className?: string;
+  buttonClassName?: string;
   textClassName?: string;
   onClick?: () => void;
 }
