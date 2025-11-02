02f9417aef0ce009caa002e477fa651a6aaabee1
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-11-03 00:01:14 +0200

    feat: replace google fonts with custom ones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,21 @@ import '@style/globals.css';
 import footerData from '@data/footerSectionData.json';
 import headerData from '@data/headerSectionData.json';
 import { type Metadata } from 'next';
-import { Play } from 'next/font/google';
+import localFont from 'next/font/local';
 import { type ReactNode } from 'react';
 
 import { Header } from '@/modules';
 import { Footer } from '@/modules/Footer';
 
-const play = Play({
-  weight: ['400', '700'],
-  subsets: ['latin', 'cyrillic'],
+const uafSans = localFont({
+  src: [
+    { path: '../fonts/UAFSans-Regular.woff2', weight: '400', style: 'normal' },
+    { path: '../fonts/UAFSans-Medium.woff2', weight: '500', style: 'normal' },
+    { path: '../fonts/UAFSans-SemiBold.woff2', weight: '600', style: 'normal' },
+    { path: '../fonts/UAFSans-Bold.woff2', weight: '700', style: 'normal' },
+  ],
   display: 'swap',
+  variable: '--font-uaf-sans',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +31,7 @@ export const metadata: Metadata = {
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <html lang="uk" className={play.className}>
+    <html lang="uk" className={uafSans.variable}>
       <body>
         <Header data={headerData} />
         {children}
