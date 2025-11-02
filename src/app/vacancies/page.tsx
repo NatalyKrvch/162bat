e4b00dbf20fadfb3d48d0afcbf7b65c0a6fd4378
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-11-02 20:12:07 +0200

    feat: add vacancies page

---
+++ src/app/vacancies/page.tsx
@@ -0,0 +1,37 @@
+import vacanciesData from '@data/rolesSectionData.json';
+
+import { Accordion, Container, SectionTitle } from '@/components';
+
+const VacanciesPage = () => {
+  const title = vacanciesData.title;
+  const vacancies = vacanciesData.items;
+
+  return (
+    <Container className="pb-12 lg:pb-20">
+      <div className="flex flex-col items-start pt-10 pb-10 lg:mb-10 lg:flex-row lg:items-center lg:justify-between lg:pt-20 lg:pb-10 xl:mb-10">
+        <SectionTitle className="xl:text-14 w-fit text-left text-4xl leading-[120%] tracking-normal lg:text-5xl lg:leading-[110%]">
+          {title}
+        </SectionTitle>
+      </div>
+
+      <div className="mx-auto flex flex-col gap-3 lg:max-w-[1000px] lg:gap-5">
+        {vacancies.map(item => (
+          <Accordion
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            content={
+              <ul className="list-disc pl-6 text-lg leading-[170%] md:text-xl lg:text-2xl">
+                {item.list.map((point, i) => (
+                  <li key={i}>{point}</li>
+                ))}
+              </ul>
+            }
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default VacanciesPage;
