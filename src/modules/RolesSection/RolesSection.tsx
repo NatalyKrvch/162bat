7a406e9347c9600ad2f30bb18bfcc951ddeea920
Dima Dolgoter <82824726+DolgoterDima@users.noreply.github.com>
2025-11-02 13:44:36 +0200

    Feature  add roles section module (#36)
    * feat: add roles section to homepage and enhance carousel functionality
    
    * refactor: improve layout and styling in RolesCard and Carousel components
    
    * test: update RolesCard tests to check rendering with title and list, and remove unused tests
    
    * fix: ensure consistent formatting by adding newline at end of file in RolesSection index
    
    * refactor: clean up formatting and improve readability in RolesSection component
    
    * refactor: enhance layout and spacing in RolesCard and RolesSection components
    
    * refactor: adjust class order for improved layout in RolesSection component

--- src/modules/RolesSection/RolesSection.tsx
+++ src/modules/RolesSection/RolesSection.tsx
@@ -9,23 +9,16 @@ const RolesSection = ({ data }: RolesSectionProps) => {
 
   const { title, readMoreText, readMoreLink, items } = data;
 
- /* <div className="w-full min-w-0 " key={index}> */ 
   const ITEMS_PER_SLIDE_ON_DESKTOP = 3;
- 
-  const slides  =
-    (items &&
-      items.map((item, index) => ( 
-       
 
-        <div className="  " key={index}>
+  const slides =
+    (items &&
+      items.map((item, index) => (
+        <div className="h-full" key={index}>
           <RolesCard title={item.title} list={item.list} />
-        </div> 
+        </div>
       ))) ||
     [];
-
-
-
-    
 
   return (
     <>
@@ -43,10 +36,10 @@ const RolesSection = ({ data }: RolesSectionProps) => {
           </Button>
         </InternalLink>
       </div>
-
-      <div className="w-full max-w-full">
+      <div className="mt-8 w-full max-w-full">
         <Carousel perView={ITEMS_PER_SLIDE_ON_DESKTOP}>{slides}</Carousel>
-      </div>    </>
+      </div>{' '}
+    </>
   );
 };
 
