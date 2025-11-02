36d95718334ea795bdd1b4923f06045a32d2d8f5
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-11-02 23:02:01 +0200

    feat: add carousel to history page

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,7 +1,13 @@
 import historyPageData from '@data/historyPageData.json';
 import Image from 'next/image';
 
-import { BasicCard, Container, SectionTitle, TitledCard } from '@/components';
+import {
+  BasicCard,
+  Carousel,
+  Container,
+  SectionTitle,
+  TitledCard,
+} from '@/components';
 
 import { type BattleJourneyItemType, type HistoryPageDataType } from './types';
 
@@ -13,11 +19,22 @@ const HistoryPage = () => {
   const todaySectionData = dataToShow.section3;
   const todaySectionPhoto = dataToShow.section3.image;
   const callToActionText = dataToShow.cta;
-  const battleJourneyGrid: BattleJourneyItemType[] = [
+  const battleJourneyGridItems: BattleJourneyItemType[] = [
     { type: 'image', src: totalWarSectionPhoto },
     { type: 'card', data: totalWarSectionData },
     { type: 'card', data: todaySectionData },
     { type: 'image', src: todaySectionPhoto },
+  ];
+  const galleryImages = [
+    '/images/history/gallery/history-1.webp',
+    '/images/history/gallery/history-2.webp',
+    '/images/history/gallery/history-3.webp',
+    '/images/history/gallery/history-4.webp',
+    '/images/history/gallery/history-5.webp',
+    '/images/history/gallery/history-6.webp',
+    '/images/history/gallery/history-7.webp',
+    '/images/history/gallery/history-8.webp',
+    '/images/history/gallery/history-9.webp',
   ];
 
   return (
@@ -31,7 +48,7 @@ const HistoryPage = () => {
 
       <div className="mx-auto max-w-[450px] sm:max-w-[600px] lg:max-w-[840px]">
         <div>
-          <BasicCard className="mb-6 px-10 py-8">
+          <BasicCard className="mb-6 px-4 py-5 lg:px-10 lg:py-8">
             <TitledCard
               titleColor="green"
               title={beginningSectionData.title}
@@ -41,7 +58,7 @@ const HistoryPage = () => {
         </div>
 
         <div className="mx-auto grid max-w-[840px] grid-cols-1 gap-6 sm:grid-cols-2">
-          {battleJourneyGrid.map((item, index) => (
+          {battleJourneyGridItems.map((item, index) => (
             <BasicCard key={index}>
               {item.type === 'image' ? (
                 item.src && (
@@ -59,7 +76,7 @@ const HistoryPage = () => {
                   titleColor="green"
                   title={item.data.title}
                   description={item.data.text}
-                  className="lg:px-8 lg:py-10"
+                  className="px-4 py-5 lg:px-8 lg:py-10"
                 />
               ) : null}
             </BasicCard>
@@ -72,6 +89,22 @@ const HistoryPage = () => {
         >
           {callToActionText}
         </BasicCard>
+      </div>
+
+      <div className="mt-12 mb-3 flex flex-col lg:mt-20 lg:mb-8 lg:justify-between xl:mt-30 xl:mb-10">
+        <SectionTitle className="xl:text-14 mb-8 w-fit text-left text-2xl leading-[120%] tracking-normal lg:text-5xl lg:leading-[110%] xl:max-w-[1000px]">
+          Галерея
+        </SectionTitle>
+      </div>
+
+      <div className="lg-mb-20 mb-10">
+        <Carousel perView={3}>
+          {galleryImages.map(item => (
+            <BasicCard key={item} hasBorder={false}>
+              <Image src={item} alt={'photo'} width={412} height={312} />
+            </BasicCard>
+          ))}
+        </Carousel>
       </div>
     </Container>
   );
