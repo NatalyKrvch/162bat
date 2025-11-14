6bd1e3bad135a0f5cabfde3e25421c91b89559fb
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-11-14 23:12:22 +0200

    test: update

--- src/components/Navigation/__test__/Navigation.test.tsx
+++ src/components/Navigation/__test__/Navigation.test.tsx
@@ -38,23 +38,6 @@ describe('Navigation', () => {
     expect(containerElement).toHaveAttribute('aria-label', 'footer menu');
   });
 
-  it('applies correct ul classes based on variant', () => {
-    const { container, rerender } = render(
-      <Navigation variant={NavigationVariant.Header}>
-        <li>Header Item</li>
-      </Navigation>,
-    );
-    const ul = container.querySelector('ul');
-    expect(ul).toHaveClass('flex flex-col gap-6.75 lg:gap-0');
-
-    rerender(
-      <Navigation variant={NavigationVariant.Footer}>
-        <li>Footer Item</li>
-      </Navigation>,
-    );
-    expect(ul).toHaveClass('mb-10 flex flex-col gap-2 sm:flex-row md:gap-x-11');
-  });
-
   it('renders children correctly', () => {
     render(
       <Navigation>
